2ed0f63f3e149518cd7482e8fa677b1b5ad6e62a
asif93-138 <asif93@student.sust.edu>
2024-01-30 00:31:45 +0600

    mongodb credentials are hidden using .env

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,7 +11,7 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://toy-marketplace:sPO2Tcf2KMAwemuS@cluster0.iuweya4.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iuweya4.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
